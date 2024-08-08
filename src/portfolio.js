f2e26253844b0174b137f6655522666a93527b23
BeyondApexTech <BeyondApexTech@gmail.com>
2024-08-08 16:48:41 +0530

    Clean up

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,13 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
-  description:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  title: "Beyond Apex Tech",
+  description: "",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Beyond Apex Tech",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
   },
 };
 

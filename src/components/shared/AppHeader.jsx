3861d79b9ebcb5d7489bad7bf6609efc551e881a
Vikash Pandey <44VikashmPandey@gmail.com>
2024-08-24 10:35:47 +0530

    About Us page and footer change

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -117,9 +117,9 @@ const AppHeader = () => {
 					<Link
 						to="/about"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
-						aria-label="About Me"
+						aria-label="About US"
 					>
-						About Me
+						About US
 					</Link>
 					<Link
 						to="/contact"
@@ -151,9 +151,9 @@ const AppHeader = () => {
 					<Link
 						to="/about"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
-						aria-label="About Me"
+						aria-label="About US"
 					>
-						About Me
+						About US
 					</Link>
 					<Link
 						to="/contact"
---
+++ src/portfolioDetails/portfolio.js
@@ -0,0 +1,35 @@
+const teamDetails = [
+    {
+        id:1,
+        name:"Vishal Surgade",
+        title:"vishalSurgade",
+        designation:"Technical Lead",
+        description : "",
+        experience : "4+"
+
+    },
+    {
+        id:2,
+        name:"Vikash Pandey",
+        title:"vikashPandey",
+        designation:"Senior Software Developer",
+        description : "Experienced Full-Stack Developer with 2+ years of expertise in creating efficient, user-friendly web applications. Proficient in front-end and back-end technologies, SDLC, and agile methodologies. Collaborative team player delivering high-quality solutions.",
+        experience : "3+"
+
+    },
+    {
+        id:3,
+        name:"Suraj Das",
+        title:"surajDas",
+        designation:"Software Developer",
+        description : "",
+        experience : "2+"
+
+    }
+]
+
+// export default teamDetails
+export {
+    teamDetails
+  };
+  
